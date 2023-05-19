ea00d64e427ef62af1a1ab7f2cae7dc7313be0e1
Darron Tacio Moraes <111967762+darronmoraes@users.noreply.github.com>
2023-05-19 14:33:43 +0530

    using rds instance locally and at ec2 server

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ CORS(app)
 
 
 # connect db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/sts_test'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root1234@sts-dev.cd6q49enrqry.ap-south-1.rds.amazonaws.com/sts_test'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/sts_test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root1234@sts-dev.cd6q49enrqry.ap-south-1.rds.amazonaws.com/sts_test'
 db.init_app(app)
 
 # Secret Key
